d6b76bd04f089a755577c08e2a1bde96689118a5
Harihara <hariharasuthan81@gmail.com>
2021-07-25 18:54:15 +0530

    second Commit

---
+++ Procfile
@@ -0,0 +1,4 @@
+release: python manage.py makemigrations --no-input
+release: python maange.py migrate --no-input
+
+web: gunicorn Profileapi.wsgi
--- Profileapi/settings.py
+++ Profileapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,3 +161,4 @@ SWAGGER_SETTINGS = {
     },
 }
 
+django_heroku.settings(locals())
